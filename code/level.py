9a06ce1d01cad8d79c863b51b8ede13f95fac8e0
Isaac <isaac.vazquez@iteso.mx>
2024-07-17 22:41:14 -0600

    imported player graphics

---
+++ code/level.py
@@ -0,0 +1,23 @@
+import pygame
+from settings import *
+from player import Player
+
+class Level:
+
+    def __init__(self):
+
+        # Get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # Sprite groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        self.player = Player((640,360), self.all_sprites)
+
+    def run(self, dt):
+        self.display_surface.fill("Black")
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update(dt)
---
+++ code/support.py
@@ -0,0 +1,14 @@
+import pygame
+from os import walk
+
+def import_folder(path):
+    surface_list = []
+
+    for __, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + "/" + image
+            print(full_path)
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list

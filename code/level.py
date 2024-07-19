4194248901820f424adc108b73f4ce6cf1411c5b
Isaac <isaac.vazquez@iteso.mx>
2024-07-19 12:57:57 -0600

    added ground

--- code/level.py
+++ code/level.py
@@ -2,26 +2,45 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
+from sprites import Generic
 
 class Level:
 
     def __init__(self):
 
-        # Get the display surface
+        # get the display surface
         self.display_surface = pygame.display.get_surface()
 
-        # Sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        # sprite groups
+        self.all_sprites = CameraGroup()
 
         self.setup()
         self.overlay = Overlay(self.player)
 
     def setup(self):
         self.player = Player((640,360), self.all_sprites)
+        Generic(
+            pos = (0,0),
+            surf = pygame.image.load("../graphics/world/ground.png").convert_alpha(),
+            groups = self.all_sprites,
+            z = LAYERS["ground"])
 
     def run(self, dt):
         self.display_surface.fill("Black")
-        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
 
         self.overlay.display()
+
+class CameraGroup(pygame.sprite.Group):
+
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+
+        for layer in LAYERS.values():
+            for sprite in self.sprites():
+                if sprite.z == layer:
+                    self.display_surface.blit(sprite.image, sprite.rect)
--- code/settings.py
+++ code/settings.py
@@ -8,3 +8,18 @@ OVERLAY_POSITIONS = {
     "tool": (80, SCREEN_HEIGHT - 65),
     "seed": (110, SCREEN_HEIGHT - 55)
 }
+
+# layers
+LAYERS = {
+    "water": 0,
+    "ground": 1,
+    "soil": 2,
+    "soil water": 3,
+    "rain floor": 4,
+    "house bottom": 5,
+    "ground plant": 6,
+    "main": 7,
+    "house top": 8,
+    "fruit": 9,
+    "rain drops": 10
+}
